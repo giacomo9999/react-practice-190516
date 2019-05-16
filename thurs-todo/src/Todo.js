73d77fb209a7ef8069f17762f6f9f8b98fefeee7
James Gary <gmarchesi888@gmail.com>
2019-05-16 15:35:10 -0400

    Added submit functionality.

--- thurs-todo/src/Todo.js
+++ thurs-todo/src/Todo.js
@@ -7,13 +7,63 @@ class Todo extends Component {
     mockData: [
       { id: 1, title: "Meet Drug Dealer", done: false, date: new Date() },
       { id: 2, title: "Fix Door Lock", done: false, date: new Date() },
-      { id: 1, title: "Install Security Camera", done: false, date: new Date() }
+      { id: 3, title: "Install Security Camera", done: false, date: new Date() }
     ]
   };
+
+  onSubmitHandle(e) {
+    e.preventDefault();
+    console.log(e.target.item.value);
+    this.setState({
+      mockData: [
+        ...this.state.mockData,
+        {
+          id: Date.now(),
+          title: e.target.item.value,
+          done: false,
+          date: new Date()
+        }
+      ]
+    });
+    e.target.item.value = "";
+  }
+
+  onDeleteHandle(e) {
+    e.preventDefault();
+  }
+
+  onEditHandle(e) {
+    e.preventDefault();
+  }
+
+  onCompleteHandle(e) {
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="container">
         <h1>I Am The To-Do Component</h1>
+        <form onSubmit={this.onSubmitHandle.bind(this)}>
+          <input type="text" name="item" className="item" />
+          <button className="btn-add-item">Add</button>
+        </form>
+        <ul>
+          {this.state.mockData.map(item => (
+            <li key={item.id}>
+              {item.title}
+              <button onClick={this.onDeleteHandle.bind(this, item.id)}>
+                Delete
+              </button>
+              <button onClick={this.onEditHandle.bind(this, item.title)}>
+                Edit
+              </button>
+              <button onClick={this.onCompleteHandle.bind(this)}>
+                Complete
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
